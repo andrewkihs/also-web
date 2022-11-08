bba4b628361c3ad635236674d2839db23bab96f6
andrewkihs <ajkihs@gmail.com>
2022-11-07 22:57:46 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+	let data 
+
+	onMount(async ()=>{
+		const response = await fetch('http://ip-api.com/json')
+		data = await response.json()
+	})
+</script>
+
+<main>
+	<h1>Coming soon</h1>
+	<div>
+		<img src="images/also-stamp.png" class = "square"/>
+	</div>
+	{#if data}
+	<div class="location-info">
+		<p>{data.lat}, {data.lon}</p>
+	</div>
+	{/if}
+
+	<!-- <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p> -->
+
+</main>
+
+<style lang="scss">
+	div {
+	perspective: 500px;	
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	// height: 100vh;
+}
+
+.location-info{
+	margin: 32px;
+}
+
+.square {
+	width: 200px;
+	height: 200px;
+	transform: rotateY(45deg);
+	animation: rotateAnimation 7s linear infinite;
+}
+
+@keyframes rotateAnimation {
+	from {transform: rotateY(0deg);}
+	to {transform: rotateY(360deg);}
+}
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+
+		h1 {
+			color: #000000;
+			text-transform: uppercase;
+			font-size: 4em;
+			font-weight: 100;
+		}
+
+		@media (min-width: 640px) {
+			max-width: none;
+		}
+	}
+</style>
